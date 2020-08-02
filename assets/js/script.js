d1531dbf1790f6e4bad7a4551474d953d3a72938
Marcelo <marcelo.silva.avello@gmail.com>
2020-08-02 02:27:45 -0400

    primera carga archivos

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+
+
+//funcion scroll
+$(function(){
+  $("a").click(function(event) {
+    // Con este if se asegura que this.hash tenga un valor antes de anular el comportamiento predeterminado
+    if (this.hash !== "") {
+      // Previene el comportamiento de click predeterminado
+      event.preventDefault();
+      // Guarda el valor del hash en una variable llamada gato
+      var gato = this.hash;
+      // Usa el método animate para animar el scroll y hacerlo de una forma suave
+      // El número opciona (800) especifica el número de milisegundos que se demorará en llegar hasta el área
+      $('html, body').animate({
+      scrollTop: $(gato).offset().top
+      }, 900, function(){
+      // Agrega hash (#) a la URL cuando haya terminado de desplazarse (comportamiento de click predeterminado)
+      window.location.hash = gato;
+      });
+    } // Fin del if
+  });
+});
+//funcin popover
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+$(function () {
+$('[data-toggle="tooltip"]').tooltip()
+})
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
